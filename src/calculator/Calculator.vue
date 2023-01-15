7af251ad79fa71151a1e30d0f19e6e70f36959b5
ALABOET <kennynotch11@gmail.com>
2023-01-15 23:37:45 +0300

    refactoring

--- src/calculator/Calculator.vue
+++ src/calculator/Calculator.vue
@@ -5,32 +5,31 @@
     </div>
     <div class="calculator_root__buttons">
       <button class="cell number" @click="inputValue(number)" v-for="number in numbers" :key="number">{{number}}</button>
-      <button class="cell operation" @click="inputOperation(operation)" v-for="operation in operations" :key="operation">{{operation}}</button>
+      <button class="cell operation" @click="inputOperation(operation)" v-for="operation in operations" :style="{'pointer-events': lastCharIsOperation ? 'none' : 'auto'}" :key="operation">{{operation}}</button>
       <button class="reset" @click="reset">reset</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 let currentInputValue = ref('0');
 let numbers = reactive(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']);
 let operations = reactive(['+', '-', '*', '-', '=']);
+
+const lastCharIsOperation = computed(() => {
+  return operations.some((value) => value === currentInputValue.value[currentInputValue.value.length-1])
+})
 
 let inputValue = (num) => {
   if (currentInputValue.value === '0') currentInputValue.value = ''
   currentInputValue.value += num
 }
 let inputOperation = (operation) => {
-  if (operation === '=') {
-    currentInputValue.value = eval(currentInputValue.value);
-    return;
-  }
+  if (operation === '=') return currentInputValue.value = eval(currentInputValue.value);
   currentInputValue.value += operation;
 }
-let reset = () => {
-  currentInputValue.value = '0'
-}
+let reset = () => currentInputValue.value = '0';
 </script>
 
 <style lang="scss">
